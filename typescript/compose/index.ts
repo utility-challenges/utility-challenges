452012f5379b5ea32e7cd0121f680c81c677c8a4
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2022-06-22 22:55:48 +0900

    clean up

--- typescript/compose/index.ts
+++ typescript/compose/index.ts
@@ -1,9 +1,1 @@
-export const compose = (...fn: ((...args: any) => any)[]): any => {
-  if (fn.length === 1) return fn[0];
-
-  return fn.reduce(
-    (a, b) =>
-      (...args) =>
-        a(b(...args))
-  );
-};
+export const compose = () => {};
--- typescript/pipe/index.ts
+++ typescript/pipe/index.ts
@@ -1,9 +1,1 @@
-export const pipe = (...fn: ((...args: any) => any)[]): any => {
-  if (fn.length === 1) return fn[0];
-
-  return fn.reduce(
-    (a, b) =>
-      (...args) =>
-        b(a(...args))
-  );
-};
+export const pipe = () => {};
