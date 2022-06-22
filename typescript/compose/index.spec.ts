78184201111902de6526d4da9b5b49e12602f05f
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2022-06-22 22:47:33 +0900

    refactor

--- typescript/compose/index.spec.ts
+++ typescript/compose/index.spec.ts
@@ -2,16 +2,17 @@ import { compose } from "./index";
 
 describe("compose", () => {
   it("pass", () => {
-    const plus10 = (n) => n + 10;
-    const double = (n) => n * 2;
+    const plus10 = (n: number) => n + 10;
+    const double = (n: number) => n * 2;
+    const plus20 = (n: number) => n + 20;
 
-    const result = compose(double, plus10)(50);
+    const result = compose(double, plus10, plus20)(50);
 
-    expect(result).toBe(120);
+    expect(result).toBe(160);
   });
 
   it("pass", () => {
-    const plus10 = (n) => n + 10;
+    const plus10 = (n: number) => n + 10;
 
     const result = compose(plus10)(50);
 
--- typescript/pipe/index.spec.ts
+++ typescript/pipe/index.spec.ts
@@ -2,16 +2,17 @@ import { pipe } from "./index";
 
 describe("pipe", () => {
   it("pass", () => {
-    const plus10 = (n) => n + 10;
-    const double = (n) => n * 2;
+    const plus10 = (n: number) => n + 10;
+    const double = (n: number) => n * 2;
+    const plus20 = (n: number) => n + 20;
 
-    const result = pipe(double, plus10)(50);
+    const result = pipe(double, plus10, plus20)(50);
 
-    expect(result).toBe(110);
+    expect(result).toBe(130);
   });
 
   it("pass", () => {
-    const plus10 = (n) => n + 10;
+    const plus10 = (n: number) => n + 10;
 
     const result = pipe(plus10)(50);
 
