812b099ebcf77fc4de017352375cd99091e3c4d4
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2022-06-23 11:02:15 +0900

    docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,72 @@
+const { description } = require('../../package')
+const { defineConfig } = require('vuepress/config')
+
+
+module.exports = defineConfig({
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'Utility Challenges',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: 'https://github.com/tyankatsu0105/utility-challenges',
+    editLinks: true,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: true,
+    nav: [
+      {
+        text: 'Guide',
+        link: '/guide/',
+      },
+      {
+        text: 'Config',
+        link: '/config/'
+      },
+      {
+        text: 'GitHub',
+        link: 'https://v1.vuepress.vuejs.org'
+      }
+    ],
+    sidebar: {
+      '/guide/': [
+        {
+          title: 'Guide',
+          collapsable: false,
+          children: [
+            '',
+            'using-vue',
+          ]
+        }
+      ],
+    }
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}) 
---
+++ docs/index.md
@@ -0,0 +1,8 @@
+---
+home: true
+heroImage: https://raw.githubusercontent.com/tyankatsu0105/utility-challenges/main/logo.png
+tagline: Let's implement a variety of utilities!
+actionText: Quick Start →
+actionLink: /guide/
+footer: Made by tyankatsu
+---
